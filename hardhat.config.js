df48f22796342ab7cd1266d5d19f9c87791cd032
Vincent <cherryiskool@gmail.com>
2025-06-14 23:07:06 +0100

    more dependencies

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,29 @@
 /** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.28",
-};
-
 require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+
+
+require("dotenv").config();
+
+const privateKey = process.env.PRIVATE_KEY;
+const endpoint = process.env.URL;
+const etherscanKey = process.env.ETHERSCAN_KEY;
+
+module.exports = {
+  solidity: {
+    version: "0.8.0"
+  },
+  networks: {
+    sepolia : {
+      url: endpoint,
+      accounts : [`0x${privateKey}`]
+    }
+  },
+	etherscan: {
+		apiKey: etherscanKey
+	}
+};
+
+  
+
